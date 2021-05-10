548718700c447219c4dadeea985247a1bee31c8d
Andrew Mitschke <ammitchky@gmail.com>
2021-05-10 13:49:45 -0500

    practice 'using' keyword and created object to call function from

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+
+namespace application_structure_checkpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var example = new exampleClass();
+            example.additionMethod();
+        }
+
+        public class exampleClass {
+            public void additionMethod() {
+                var first = 3;
+                var second = 7;
+                var result = first + second;
+
+                result.Should().Be(10);
+            }
+        }
+    }
+}
